17e8b69f92a503ca29b236ad4a15ff5e87d7fee1
alhaithamgenshin4-source <alhaithamgenshin4@gmail.com>
2025-12-24 18:21:19 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
+let themes = [];
 
+fetch("themes.json")
+  .then(r => r.json())
+  .then(d => {
+    themes = d.themes;
+    applyTheme(themes[0]);
+  });
+
+function applyTheme(theme) {
+  document.body.style.backgroundImage =
+    `url(${theme.background})`;
+  localStorage.setItem("theme", theme.id);
+}
+
+function openStore() {
+  const store = document.getElementById("store");
+  store.innerHTML = "<h2>Store de Temas</h2>";
+
+  themes.forEach(t => {
+    const div = document.createElement("div");
+    div.innerHTML = `
+      <p>${t.name}</p>
+      <button onclick="applyTheme(themes.find(x=>'${t.id}'===x.id))">
+        Aplicar
+      </button>
+    `;
+    store.appendChild(div);
+  });
+
+  store.classList.remove("hidden");
+}
