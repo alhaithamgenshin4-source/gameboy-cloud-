b22c3f300b58f33896bee31cf0023ae4e64b2694
alhaithamgenshin4-source <alhaithamgenshin4@gmail.com>
2025-12-24 18:23:15 -0300

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,1 +1,9 @@
-
+document.addEventListener("keydown", e => {
+  if (e.key === "F12") {
+    const code = prompt("Código Admin:");
+    if (code === "7777") {
+      alert("Modo Admin Ativado");
+      localStorage.setItem("admin", "true");
+    }
+  }
+});
